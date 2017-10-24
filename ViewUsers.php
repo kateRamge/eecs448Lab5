8e598ec5dca7145af02e9f17573ea60cc5d77abb
Katherine Ramge <kate.ramge@gmail.com>
2017-10-24 13:14:43 -0500

    working on part 5

--- ViewUsers.php
+++ ViewUsers.php
@@ -9,10 +9,14 @@ if($mysqlUsers->connect_error) {
 if($mysqlUsers->query("SELECT user_id FROM Users WHERE user_id = " . $userAuthorID)->num_rows == 0) {
   //new user
 }
-else {
-  //existing user
-  //TODO: create table outline
-  //TODO: create loop thingy
+
+//existing user
+//TODO: create table outline
+//TODO: create loop thingy
+echo "<table>";
+while($row = mysql_fetch_array(mysql_query("SELECT user_id FROM Users"))) {
+  echo "<tr><td>" .$row['user_id'] . "</td></tr>";
 }
+echo "</table>";
 
 ?>
