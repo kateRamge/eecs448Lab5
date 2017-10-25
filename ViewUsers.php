63fd8dfe721dd03d4c04d4eca21f0a3479494d59
Katherine Ramge <kate.ramge@gmail.com>
2017-10-25 10:24:43 -0500

    This file is working 100%git add ViewUsers.php!

--- ViewUsers.php
+++ ViewUsers.php
@@ -1,22 +1,23 @@
 <?php
 //help from stackoverflow
-//stackoverflow.com/questions/8437685/display-sql-table-in-php
-$mysqlUsers = new mysqli("mysql.eecs.ku.edu", "kramge", 'P@$$word123', "Users");
+//https://stackoverflow.com/questions/17902483/show-values-from-a-mysql-database-table-inside-a-html-table-on-a-webpage
+$servername = 'mysql.eecs.ku.edu';
+$username = 'kramge';
+$password = 'P@$$word123';
+$database = 'kramge';
+$mysqlUsers = new mysqli($servername, $username, $password, $database);
 
 if($mysqlUsers->connect_error) {
-  die("Connection failedL " . $mysqlUsers->connect_error);
-}
-if($mysqlUsers->query("SELECT user_id FROM Users WHERE user_id = " . $userAuthorID)->num_rows == 0) {
-  //new user
+  die("Connection failed " . $mysqlUsers->connect_error);
 }
 
-//existing user
-//TODO: create table outline
-//TODO: create loop thingy
-echo "<table>";
-while($row = mysql_fetch_array(mysql_query("SELECT user_id FROM Users"))) {
-  echo "<tr><td>" .$row['user_id'] . "</td></tr>";
+$sql = "SELECT * FROM Users";
+$result = mysqli_query($mysqlUsers, $sql);
+echo "<table border='3'>";
+echo "<tr><td> Users </td></tr>";
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr><td>" . $row['user_id'] . "</td></tr>";
 }
 echo "</table>";
-
+mysqli_close($mysqlUsers);
 ?>
